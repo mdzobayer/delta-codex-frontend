ea3fc4b1e676f23bb82c1035c4445c315d0e5385
mdzobayer <mdzobayer@rocketmail.com>
2020-03-13 19:55:45 +0600

    add routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContestListComponent } from './contest-list/contest-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contest-list',
+    component: ContestListComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HomeComponent,
+  ContestListComponent,
+  PageNotFoundComponent
+];
