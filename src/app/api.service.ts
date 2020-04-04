f26b0fbec02a0142083c6fbff09313e7b947ea00
mdzobayer <mdzobayer@rocketmail.com>
2020-04-04 13:26:53 +0600

    receive typed response from request

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CodeforcesUrls } from '../assets/codeforces/const-urls';
+import { ContestListResp } from './Models/contestlistresp'
 
 
 @Injectable({
@@ -11,6 +12,6 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getContestList() {
-    return this.http.get(CodeforcesUrls.contestListWithoutGym);
+    return this.http.get<ContestListResp>(CodeforcesUrls.contestListWithoutGym);
   }
 }
