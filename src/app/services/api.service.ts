fc18d8e9e4867aed2cda31a6a94d52a3f7eb3cc1
mdzobayer <mdzobayer@rocketmail.com>
2020-04-05 09:39:52 +0600

    move service to services folder

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CodeforcesUrls } from '../../assets/codeforces/const-urls';
+import { ContestListResp } from '../models/contestlistresp'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getContestList() {
+    return this.http.get<ContestListResp>(CodeforcesUrls.contestListWithoutGym);
+  }
+}
